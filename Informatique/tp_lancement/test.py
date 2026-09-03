95382bffb1444b5bcbb74a0a49ad44e1d2032544
skoyo-png <dacruzjocelin@gmail.com>
2026-09-03 10:00:55 +0200

    Premier commit

---
+++ Informatique/tp_lancement/test.py
@@ -0,0 +1,6 @@
+def hello(name: str) -> None :   
+    print(f"Hello {name} ! ") 
+print("Hello World ! ") 
+print(f"Script name : {__name__}") 
+if __name__ == "__main__" : 
+    hello("Jeune Trublion")
