a20a931780faa5b5bac8c7133a82d458beadb2a9
skoyo-png <dacruzjocelin@gmail.com>
2026-09-11 14:00:26 +0200

    Exo 4

--- Informatique/tp_01/ex4.py
+++ Informatique/tp_01/ex4.py
@@ -1,6 +1,11 @@
-N = int(input("Combien d'approximations en tout ? "))
-for _ in range(N):
-    if N < 0:
-        print("Le nombre d'approximations ne peut pas être négatif.")
-    else:
-        
+N = int(input("Entrer un entier N: "))
+i = 2
+k = 2
+pi = 3
+terme = 1
+while(terme != N):
+    pi = pi+(4*(-1)**k) / (i*(i+1)*(i+2))
+    i += 2
+    k += 1
+    terme += 1
+print(f"La valeur de pi est : {round(pi,terme)}")
