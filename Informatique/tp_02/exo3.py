3cfd94aaafc71a46d1c463dcb90c2d13c386d746
skoyo-png <dacruzjocelin@gmail.com>
2026-09-18 13:52:17 +0200

    Exo3

--- Informatique/tp_02/exo3.py
+++ Informatique/tp_02/exo3.py
@@ -1,13 +1,17 @@
 import random 
 n = random.randint(3, 99)
 tab = [random.randint(0, 500) for i in range(n)]
+double = []
 
 def similaire(tab):
-    for i in range(len(tab)):
+    for i in range(0, len(tab)-1):
         for j in range(i + 1, len(tab)):
-            if tab[i] == tab [j] and i != j:
-                return ("Il y a des éléments similaires dans le tableau.")
-            return ("Il n'y a pas d'éléments similaires dans le tableau.")
+            if tab[i] == tab [j]:
+                double.append(tab[i])
+    if (len(double) == 0):
+        return "Aucun doublon"
+    else:
+        return "Il y a des doublons"
+        return double
 
-print(tab)
 print(similaire(tab))
